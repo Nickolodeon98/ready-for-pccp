a62f40c90acb8d6307359112fd55e1e9dbe0578e
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-28 00:21:20 +0900

    feat: 프로그래머스 예산 문제 완전탐색으로 풀려다가 실패

--- ready-for-pccp/src/main/java/budget/P12982.java
+++ ready-for-pccp/src/main/java/budget/P12982.java
@@ -1,7 +1,8 @@
 package budget;
 
 public class P12982 {
-    /* 각 부서에 따라 필요한 금액들 중 몇 개를 더해야 배정된 예산을 넘지 않으면서 지원하는 부서의 개수가 최대가 되는지 구한다. */
+    /* 각 부서에 따라 필요한 금액들 중 몇 개를 더해야 배정된 예산을 넘지 않으면서 지원하는 부서의 개수가 최대가 되는지 구한다.
+    * 1. 완전 탐색으로 해본 결과, 완전 탐색으로는 풀 수 없다.*/
 
     public int bigEfficiency(int[] d, int budget) {
         int max = 0;
@@ -28,20 +29,32 @@ public class P12982 {
         return count;
     }
 
+    public int findEfficiency(int[] d, int budget) {
+        int firstOption = findOptimum(d, budget, 0, 0, 0);
+        int secondOption = findOptimum(d, budget, 1, 0, 0);
+        return firstOption > secondOption ? firstOption : secondOption;
+    }
     public int findOptimum(int[] d, int budget, int option, int currentIdx, int count) {
         if (budget < 0) return count-1;
+        if (budget == 0) return count;
         if (currentIdx == d.length) return count;
+        if (count == d.length) return count;
 
         if (option == 1) {
             budget -= d[currentIdx];
             count++;
         }
-        int max = 0;
-        for (int i = 0; i < 1; i++) { // 아예 안 챙겨주거나 지원해주거나 두 옵션 중 하나이다.
-            int potential = findOptimum(d, budget, i, ++currentIdx, count);
-            if (potential > max) max = potential;
-        }
+        currentIdx++;
+        int firstOption = findOptimum(d, budget, 0, currentIdx, count);
+        int secondOption = findOptimum(d, budget, 1, currentIdx, count);
+        int max = firstOption > secondOption ? firstOption : secondOption;
 
         return max;
+    }
+
+    public static void main(String[] args) {
+        P12982 p12982 = new P12982();
+        int[] d = {1,3,2,5,4};
+        System.out.println(p12982.findEfficiency(d, 9));
     }
 }
